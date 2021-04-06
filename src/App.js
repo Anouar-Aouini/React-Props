9bfa5bbc0e420f4c9cf9b68758b08daef3c0b0a1
Anouar-Aouini <anoirawini1571@gmail.com>
2021-04-06 22:24:56 +0100

    React Props

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react'
+import Profile from '../src/profile/Profile.js'
+
+function App() {
+  return (
+    <div className="App">
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ src/profile/Profile.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Profile = (fullName, bio, profession) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
