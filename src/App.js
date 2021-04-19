02480752c67cdabe462f88d1dfb787b39b36449b
Anouar-Aouini <anoirawini1571@gmail.com>
2021-04-19 19:53:22 +0100

    React props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
-import React from 'react'
-import Profile from '../src/profile/Profile.js'
+import React from 'react';
+import Profile from '../src/profile/Profile'
 
 function App() {
+  const alertMyInput = props => alert(`${props}`);
   return (
     <div className="App">
-    
+      < Profile fullName = "React-JS"
+      bio = "React is an open-source, front end, JavaScript library for building user interfaces or UI components. It is maintained by Facebook and a community of individual developers and companies. React can be used as a base in the development of single-page or mobile applications."
+      profession = "JavaScript library for creating user interfaces."
+      alertMyInput={alertMyInput}>
+      <img src="https://www.valuecoders.com/blog/wp-content/uploads/2018/06/1_i_Vj3gs7QO9fjcxELK2c2g.png" alt="react-logo" /></ Profile>
     </div>
   );
 }
-
 export default App;
--- src/profile/Profile.js
+++ src/profile/Profile.js
@@ -1,9 +1,29 @@
 import React from 'react'
+import PropTypes from "prop-types";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap'
+import './Profile.css'
 
-export const Profile = (fullName, bio, profession) => {
-    return (
-        <div>
-            
-        </div>
-    )
+export default function Profile(props) {
+    return ( < div className="DIV" style = {
+            {display: "flex",flexDirection: "column",}}> {
+            props.children
+        } <Button onClick = {
+            () => props.alertMyInput(`My name is    :   ${props.fullName}
+
+Bio    :     ${props.bio}
+
+Profession     :     ${props.profession}`)
+        } > ClickMe </Button> </div>
+    );
+};
+Profile.defaultProps = {
+    fullName: "React",
+    bio: "React A JavaScript library for building user interfaces .",
+    profession: "JavaScript library ."
+};
+Profile.propTypes = {
+    fullName: PropTypes.string.isRequired,
+    bio: PropTypes.string.isRequired,
+    profession: PropTypes.string.isRequired,
 }
